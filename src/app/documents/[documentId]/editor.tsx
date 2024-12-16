bb345634934d47216dd11332281a661f63f7b1f5
zczhao1992 <zczhao1992@163.com>
2024-12-16 22:20:07 +0800

    change md

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -44,6 +44,7 @@ export const Editor = ({ initialContent }: EditorProps) => {
   });
 
   const editor = useEditor({
+    autofocus: true,
     immediatelyRender: false,
     onCreate({ editor }) {
       setEditor(editor);
